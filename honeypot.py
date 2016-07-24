dc6f13d1a3d0e268df14c7daee633ebc55f8abc1
Christian Weber <webdog@github.com>
2016-07-23 22:56:18 -0500

    Initial commit for app

---
+++ honeypot.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+
+#Get the hooks we want to monitor from the environment variables
+def hk_envs():
+	vals = {}
+	for k, v in os.environ.items():
+		if "hk-" in k and  v == "True":
+			vals[k] = v
+		else:
+			pass
+	return vals
+
+# Assign to variable so we only call the environment once
+hks_true = hk_envs()
+
+
+@app.route('/')
+def hello_world():
+	return "Nothing to see here. Move along."
+
+@app.route('/receive', methods = ['POST'])
+def process_post():
+	headers = request.headers
+	for k, v in headers.items():
+		if v not in hks_true:
+			pass
+		else:
+			message = request.to_json()
+			print(message)
+
+@app.route('/receive', methods = ['GET'])
+def receive_get():
+	return "Nothing to see here. Move along."
+
+if __name__ == '__main__':
+	app.run()
+
+
